4c66ce5f7b9af8d074a119ca58ee7570722d3409
wjohn1483 <wjohn1483@yahoo.com.tw>
2020-02-28 20:33:21 +0800

    Overwrite folder instead of copy

--- .travis/push.sh
+++ .travis/push.sh
@@ -10,7 +10,7 @@ setup_git_folder() {
 
 commit_website_files() {
     git checkout master
-    cp -r ../_site/* ./
+    rsync -a -v --delete-after ../_site/* ./
     git status
     git add .
     git commit -m "Travis build: $TRAVIS_BUILD_NUMBER"
