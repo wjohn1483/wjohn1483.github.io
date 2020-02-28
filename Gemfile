db61032d0cf9efb4d703ac54c12116d824825d0c
wjohn1483 <wjohn1483@yahoo.com.tw>
2020-02-28 19:05:49 +0800

    Upload site

--- Gemfile
+++
@@ -1,8 +0,0 @@
-source 'https://rubygems.org'
-
-gem "jekyll"
-gem "jekyll-text-theme"
-
-group :jekyll_plugins do
-    gem "jekyll-postfiles"
-end
--- assets/search.js
+++ assets/search.js
@@ -1,3 +1,1 @@
----
----
-{%- include search-providers/default/search-data.js -%}
+window.TEXT_SEARCH_DATA={'posts':[{'title':"Create website",'url':"/2020/02/27/create-website/"}]};
