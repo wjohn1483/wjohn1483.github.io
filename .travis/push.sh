132d17707fbbe089d88ced980395edd22d336dd3
wjohn1483 <wjohn1483@yahoo.com.tw>
2020-02-28 20:05:07 +0800

    Update travis push

--- .travis/push.sh
+++ .travis/push.sh
@@ -1,25 +1,28 @@
 #!/bin/bash
 
-setup_git() {
+setup_git_folder() {
     git init
     git config --global user.email "wjohn1483@yahoo.com.tw"
     git config --global user.name "wjohn1483"
+    git remote add origin https://${GITHUB_TOKEN}@github.com/wjohn1483/wjohn1483.github.io.git > /dev/null 2>&1
+    git pull origin master
 }
 
 commit_website_files() {
     git checkout master
+    cp -r ../_site/* ./
     git add .
     git commit -m "Travis build: $TRAVIS_BUILD_NUMBER"
 }
 
 upload_files() {
-    git remote add origin https://${GITHUB_TOKEN}@github.com/wjohn1483/wjohn1483.github.io.git > /dev/null 2>&1
     git status
     #git push --quiet --set-upstream origin master
 }
 
-cd ./_site
-setup_git
+mkdir folder_to_push
+cd folder_to_push
+setup_git_folder
 commit_website_files
 upload_files
 
