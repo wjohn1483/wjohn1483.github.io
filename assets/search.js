54e1f629544590981bfc4b53c2270b49f4493170
wjohn1483 <wjohn1483@yahoo.com.tw>
2020-02-29 06:02:59 +0000

    Travis build: 23

--- assets/search.js
+++ assets/search.js
@@ -1,1 +1,1 @@
-window.TEXT_SEARCH_DATA={'posts':[{'title':"如何使用Jekyll建立網站在Gihub Page上",'url':"/2020/02/28/create-website-by-Jekyll/"}]};
+window.TEXT_SEARCH_DATA={'posts':[{'title':"如何使用Jekyll建立網站在Github Page上",'url':"/2020/02/28/create-website-by-Jekyll/"}]};
