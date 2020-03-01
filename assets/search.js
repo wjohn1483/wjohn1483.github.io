958f7a42ffb4b72d961ca648d75d784a7065d89b
wjohn1483 <wjohn1483@yahoo.com.tw>
2020-03-01 15:11:42 +0000

    Travis build: 25

--- assets/search.js
+++ assets/search.js
@@ -1,1 +1,1 @@
-window.TEXT_SEARCH_DATA={'posts':[{'title':"如何使用Jekyll建立網站在Github Page上",'url':"/2020/02/28/create-website-by-Jekyll/"}]};
+window.TEXT_SEARCH_DATA={'posts':[{'title':"如何使用Jekyll建立網站在Github Page上",'url':"/2020/02/28/create-website-by-Jekyll/"},{'title':"Self-Supervised Generalisation with Meta Auxiliary Learning",'url':"/2020/03/01/self-supervised-generalisation-with-meta-auxiliary-learning/"}]};
